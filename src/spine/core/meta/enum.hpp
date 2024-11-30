fc86718e6004a136f37b199efc8e1c877e958860
Stan Verschuuren <31728406+s-t-a-n@users.noreply.github.com>
2024-11-30 10:58:41 +0100

    core/meta: expand enum
    - Add `has_flag` helper for verbose checking if a flag is set in a
      bitmask

--- src/spine/core/meta/enum.hpp
+++ src/spine/core/meta/enum.hpp
@@ -27,6 +27,13 @@ constexpr uint16_t U16(T idx) {
 }
 
 template<typename Enum>
+/// Returns true if a flag `flag` is contained in a bitmask `value`
+constexpr std::enable_if_t<std::is_enum_v<Enum>, bool> has_flag(Enum value, Enum flag) {
+    using T = std::underlying_type_t<Enum>;
+    return (static_cast<T>(value) & static_cast<T>(flag)) != 0;
+}
+
+template<typename Enum>
 constexpr typename std::enable_if_t<std::is_enum_v<Enum>, Enum> operator|(Enum lhs, Enum rhs) {
     using T = std::underlying_type_t<Enum>;
     return static_cast<Enum>(static_cast<T>(lhs) | static_cast<T>(rhs));
