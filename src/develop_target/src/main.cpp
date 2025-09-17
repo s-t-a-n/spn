6560464bf8899560e558df79337af64675538451
Stan Verschuuren <31728406+s-t-a-n@users.noreply.github.com>
2025-09-18 01:18:30 +0200

    src/develop_target: stub for IDE's
    - This is a stub target with a cpp file that includes all headers of
      the library, to be used by IDE's like Clion, which if pointed at
      this directory will have all files indexed instead of every project
      separately.

---
+++ src/develop_target/src/main.cpp
@@ -0,0 +1,41 @@
+#include <zephyr/kernel.h>
+
+// allocation
+#include "spn/allocation/heap.hpp"
+#include "spn/allocation/pool.hpp"
+#include "spn/allocation/shared_ptr.hpp"
+#include "spn/allocation/slab.hpp"
+#include "spn/allocation/unique_ptr.hpp"
+
+// containers
+#include "spn/containers/callback.hpp"
+#include "spn/containers/mpmc_deque.hpp"
+#include "spn/containers/mpmc_queue.hpp"
+#include "spn/containers/mpsc_queue.hpp"
+#include "spn/containers/poll.hpp"
+
+// core
+#include "spn/core/enum_flags.hpp"
+#include "spn/core/types.hpp"
+
+// debugging
+#include "spn/debugging/assert.hpp"
+
+// example_lib
+#include "spn/example_lib/example_lib.hpp"
+
+// logging#include "spn/logging/logging.hpp"
+#include "spn/logging/memory.hpp"
+
+// threading
+#include "spn/threading/condition.hpp"
+#include "spn/threading/event.hpp"
+#include "spn/threading/lockguard.hpp"
+#include "spn/threading/mutex.hpp"
+#include "spn/threading/semaphore.hpp"
+#include "spn/threading/thread.hpp"
+#include "spn/threading/work.hpp"
+
+// note: this is a pure stub for ide's like clion which only lint/analyze files which are part of a target.
+
+int main(void) { return 0; }
