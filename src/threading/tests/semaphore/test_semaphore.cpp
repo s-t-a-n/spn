2c1803f5b8d8412ac6050264e81413ea1ea7baac
Stan Verschuuren <31728406+s-t-a-n@users.noreply.github.com>
2025-10-23 00:42:02 +0200

    threading: minor cleanup, missing yaml files
    - Fix bad testnames; need to prefix with test_
    - Add missing testcase.yaml/sample.yaml
    - Raise stacksize for thread test prj.conf as it was causing trouble on
      64-bit platforms

--- src/threading/tests/semaphore/test_semaphore.cpp
+++ src/threading/tests/semaphore/test_semaphore.cpp
@@ -7,7 +7,7 @@ namespace {
 
 ZTEST_SUITE(semaphore_suite, NULL, NULL, NULL, NULL, NULL);
 
-ZTEST(semaphore_suite, semaphore_basic_operations) {
+ZTEST(semaphore_suite, test_basic_operations) {
     spn::Semaphore<1, 5> sem;
 
     zassert_equal(sem.count(), 1, "initial count should match template parameter");
@@ -21,7 +21,7 @@ ZTEST(semaphore_suite, semaphore_basic_operations) {
     zassert_equal(sem.count(), 1, "count should be incremented after give");
 }
 
-ZTEST(semaphore_suite, semaphore_take_timeout) {
+ZTEST(semaphore_suite, test_take_timeout) {
     spn::Semaphore<0, 5> sem;
 
     int result = sem.take(K_NO_WAIT);
